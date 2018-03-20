6967dd755bb38e575e27368a1ca527baf77f35f3
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2018-03-19 20:07:37 -0400

    added required plugins to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LocationTrackerProvider } from '../providers/location-tracker/location-tracker';
+import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    LocationTrackerProvider
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    LocationTrackerProvider,
+    BackgroundGeolocation,
+    Geolocation
   ]
 })
 export class AppModule {}
